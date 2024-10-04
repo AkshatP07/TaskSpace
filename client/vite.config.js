54f6f29b11f90bcb662b50e05ae6fbafc1b87c64
AkshatP07 <145288745+AkshatP07@users.noreply.github.com>
2024-10-04 11:13:26 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This is important
+    emptyOutDir: true
+  },
+  base: './', // This ensures relative path routing for production
+});
